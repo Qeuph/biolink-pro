548c2d938ce3e9541d675328cedb6a4330cda6af
Qeuph <nuke365873@Gmail.com>
2025-12-29 19:17:20 +0500

    Initialize Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        zinc: {
+          950: '#09090b',
+          900: '#18181b',
+          800: '#27272a',
+          700: '#3f3f46',
+          500: '#71717a',
+        },
+        indigo: {
+          500: '#6366f1',
+          600: '#4f46e5',
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      }
+    },
+  },
+  plugins: [],
+}
