5fec06702debda79bb40dc11efe5df9cf317b016
Qeuph <nuke365873@Gmail.com>
2025-12-29 19:23:11 +0500

    Update Firebase configuration with new credentials

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyDIuvZF7X3fTKn6QjCFjneVWe4em9dJLGM",
+  authDomain: "biolink-pro-c6e8e.firebaseapp.com",
+  projectId: "biolink-pro-c6e8e",
+  storageBucket: "biolink-pro-c6e8e.firebasestorage.app",
+  messagingSenderId: "865275605116",
+  appId: "1:865275605116:web:60698d09ca8c676f1899f9",
+  measurementId: "G-CY7P1TLMTT"
 };
 
 const app = initializeApp(firebaseConfig);
